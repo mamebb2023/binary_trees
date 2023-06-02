6fc931b5adaa06872750bb87a955a027ce7fb074
Mohammednur <mamebb2023@gmail.com>
2023-06-02 10:59:51 +0300

    Binary tree Postorder Traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,10 +8,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if ((tree != NULL) && (fucn != NULL))
+	if ((tree != NULL) && (func != NULL))
 	{
 		binary_tree_preorder(tree->left, func);
-		binary_tree_preordre(tree->right, func);
+		binary_tree_preorder(tree->right, func);
 		func(tree->n);
 	}
 }
